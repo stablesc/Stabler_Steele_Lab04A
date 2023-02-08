757f39f9e850d356151b61ee9913633e9699b60c
Steele <stablesc@mail.uc.edu>
2023-02-07 23:15:51 -0500

    project finished

---
+++ src/StablerStudentInfo.java
@@ -0,0 +1,10 @@
+public class StablerStudentInfo {
+    public static void main(String[] args)
+
+    {
+        System.out.println("Steele C. Stabler");
+        System.out.println("French");
+        System.out.println("2018");
+
+    }
+}
